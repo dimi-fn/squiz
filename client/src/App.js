fef17ca804bd4195ea27ada7fce84ea4b955ffc1
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-18 18:31:01 +0300

    set up docker for server-socket-client
    Co-authored-by: HevvsOlivia <HevvsOlivia@users.noreply.github.com>
    Co-authored-by: kelvin6118 <kelvin6118@users.noreply.github.com>
    Co-authored-by: IKENNA AGULOBI <ike-agu@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,39 @@
 import './App.css';
 import io from "socket.io-client";
+// import React, {useState, useEffect} from 'react';
 import React, {useState} from 'react';
 const serverEndpoint = "http://127.0.0.1:5000";
+const socket = io.connect(serverEndpoint);
 
 function App() {
 
   const [socket, setSocket] = useState("");
 
   const componentDidMount = () => {
-    const socket = io(serverEndpoint);
+    
     console.log(socket)
     setSocket({ socket });
+
+    
   }
     
-  const componentWillUnmount = () => {    
-      socket.disconnect();
-  };
+  // const componentWillUnmount = () => {    
+  //     socket.disconnect();
+  //     // socket.manager.onClientDisconnect(socket);
+  //     console.log(socket)
+  //     // useEffect(socket.disconnect(),[]);      
+  // };
 
   return (
       
     <>
     <h1>Squiz team</h1>
-    componentDidMount
+    {/* {componentDidMount()} */}
+    {/* {componentWillUnmount()} */}
     </>
   );
 };
+
 
 // function App() {
 //   return (
--- server/socketIO/server.js
+++ server/socketIO/server.js
@@ -1,25 +1,47 @@
-const app = require("express")();
-/* const app= require('../api'); */
-const server = require("http").createServer(app);
-const io = require("socket.io")(server) // integrate our http server with a new instance of socket.io
+/* const app = require("express")(); */
+const express = require("express");
+const app = express();
+const http = require("http");
 
-// socket connection will go here
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+
+app.get('/', (req, res)=> res.send('Hello from the SocketIO server!'));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+
+// const app= require('../api'); 
+// const server = require("http").createServer(app);
+// const cors = require("cors");
+
+// const io = require("socket.io")(server, {    
+//     cors: {origin: "https://localhost:3000",
+//      methods:["GET","POST"],}}
+//     ) // integrate our http server with a new instance of socket.io
+
+// app.use(cors());
+
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Express for SocketIO is running on port ${port}`))
+server.listen(port, () => console.log(`Express for SocketIO is running on port ${port}!`))
 
-// server.get('/', (req, res) => res.send('Hello from Squiz with SocketIO!!!'))
-
-
-io.on('connection', socket => {
+io.on('connection', (socket) => {
     console.log("'Ello, who's this we got here?") // runs when client first connects
+    console.log(`${socket}`);
+    console.log(`${socket.id}`);
 
-    socket.on("disconnect", socket => { // runs when client disconnects
-        console.log("K bye then");
-    });
+    // socket.on("disconnect", (socket) => { // runs when client disconnects
+    //     console.log("K bye then");
+    // });
 });
 
 
-// server.listen(port, () => {
-//     console.log(`Open for play on port ${port} by SocketIO!`)
-// });
+
