f6b81159e0596108eda06d4ec39c1c3266107bf3
Yan Wong <kelvinwong26546@gmail.com>
2022-04-19 23:35:31 +0800

    displaying username not finished

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Lobby = () => {
+    const user = useSelector(state => state.UserName);
+
+    const renderUser = () => {user.map(e => <div>{e}</div>)}
 
     return(
     <>
-    
+    <h1>Lobby</h1>
+    {
+        
+    }
     </>
     )
 }
