8167c713ab61a80e5d47fb71d92a48325702432c
Dimitris Fanis <56633241+dimi-fn@users.noreply.github.com>
2022-04-19 18:41:35 +0300

    Revert "set up QuizForm component"

--- client/src/pages/createGame.js
+++ client/src/pages/createGame.js
@@ -1,19 +1,9 @@
-// import React, {useState} from 'react';
 import React from 'react';
 
-
-import {QuizForm} from "../components/QuizForm";
-
-function CreateGame() {
-  
-    return(
+const CreateGame = () => (
     <>
-        <main>
-            <QuizForm/>
-        </main>
     
     </>
-    )
-};
+)
 
- export default CreateGame;
+export default CreateGame;
