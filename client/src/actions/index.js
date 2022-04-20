87a4116b018aae40b635bdf24f9f0e561911e31e
Yan Wong <kelvinwong26546@gmail.com>
2022-04-20 22:51:31 +0800

    display question and answers on game page

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-//import axios from 'axios';
+import axios from 'axios';
 
 const setRoomID = roomID => ({ type: 'CREATE_ROOM', payload: roomID });
 
--- client/src/components/QuizQuestions/index.js
+++ client/src/components/QuizQuestions/index.js
@@ -1,47 +1,90 @@
 import React, {useState, useEffect} from 'react';
-
+import { useSelector } from 'react-redux';
+// import axios from 'axios';
 
 
 
 const QuizQuestions = () => {
+    const [question, setQuestion] = useState('');
+    const [questions, setQuestions ] = useState();
+    //const [answers, setAnswers] = useState('');
+    const NumOfQuestion = useSelector(state => state.questions);
+    const category = useSelector(state => state.category);
+    const level =  useSelector(state => state.difficulty);
+    let result = [];
+    
 
-const [question, setQuestion] = useState('');
+    const fetchQuestions = async () => {
+        const response = await fetch (`https://opentdb.com/api.php?amount=${NumOfQuestion}&category=${category}&difficulty=${level}&type=multiple`);
+        //const response = await fetch (`https://opentdb.com/api.php?amount=2&category=9&difficulty=easy&type=multiple`);
+        const data = await response.json();
+        data.results.forEach((data) => {
+            setQuestion((prevState) => ({ ...prevState, [data.question] : [data.correct_answer] [data.incorrect_answers]}))
+            result.push(data);
+        })
+        console.log(`Data is: ${data}`);
+        setQuestions(result);
+    }
 
-const fetchQuestions = async () => {
-    // const response = await fetch (`https://opentdb.com/api.php?amount=${form.questionNum.value}&category=${form.category.value}&difficulty=${form.level.value}$type=multiple`);
-    const response = await fetch (`https://opentdb.com/api.php?amount=2&category=9&difficulty=easy&type=multiple`);
-    const data = await response.json();
-    data.results.forEach((data) => {
-        setQuestion((prevState) => ({ ...prevState, [data.question] : [data.correct_answer] [data.incorrect_answers]}))
-    })
-    console.log(data);
-}
+    useEffect(() => {
+        fetchQuestions();
+    }, [])
+    console.log(questions)
+    //console.log(questions);
+    const renderQuestion = () => {if(questions != undefined){return questions.map((Q) => (
+      <>
+        <strong>
+          <p>{Q.question}</p>
+        </strong>
+        {renderAnswer(Q)}
+      </>
+    ));}}
 
+    // shuffle our array below 'allAnswers' containing all candidate answers                    
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
 
-const allQuestions = Object.keys(question).map((ques) => {
-    return (
-            <option key={ques} value={ques}>
-            {question[ques]}
-            </option>
-    )
-});
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
 
-useEffect(() => {
-    fetchQuestions();
-}, [])
+            // Pick a remaining element.
+            randomIndex = parseInt(Math.floor(Math.random() * currentIndex));
+            currentIndex--;
 
-    return (
-        <section role="">
-            <>
-            <h2>You Quiz Game:</h2>
-            <p id="question" name="question" className="question">
-                {allQuestions}
-            </p>
-            
-            </>
-        </section>
-    )
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
 
+        return array;
+    }
+
+    const renderAnswer = (Q) => {if(questions != undefined){
+        const allAnswers = Q.incorrect_answers;
+        allAnswers.push(Q.correct_answer);
+        const shuffledAnswered = shuffle(allAnswers);
+        let uniqueShuffled = [...new Set(shuffledAnswered)]; // https://stackoverflow.com/a/33121880
+        console.log(uniqueShuffled);
+        // for(i=0; i<=allAnswers.length; i++){
+        //     return allAnswers[i]
+        // }
+        return uniqueShuffled.map( (Q) =>
+        <p key={Q.id}>{Q}</p>
+        );}}
+    
+    
+        return (
+            <section role="">
+                <>
+                <h2>You Quiz Game:</h2>
+                <p id="question" name="question" className="question">
+                    {renderQuestion()}
+                    
+                </p>
+                
+                </>
+            </section>
+        )
 }
 
 
