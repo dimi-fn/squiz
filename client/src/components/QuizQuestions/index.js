37099ca43356d3e8f83cdbe175123dbaf23a9638
Yan Wong <kelvinwong26546@gmail.com>
2022-04-20 23:58:08 +0800

    answer input

--- client/src/components/QuizQuestions/index.js
+++ client/src/components/QuizQuestions/index.js
@@ -5,23 +5,24 @@ import { useSelector } from 'react-redux';
 
 
 const QuizQuestions = () => {
-    const [question, setQuestion] = useState('');
     const [questions, setQuestions ] = useState();
     //const [answers, setAnswers] = useState('');
     const NumOfQuestion = useSelector(state => state.questions);
     const category = useSelector(state => state.category);
     const level =  useSelector(state => state.difficulty);
-    let result = [];
+    let result;
+    // let result=[]; 
     
 
     const fetchQuestions = async () => {
         const response = await fetch (`https://opentdb.com/api.php?amount=${NumOfQuestion}&category=${category}&difficulty=${level}&type=multiple`);
         //const response = await fetch (`https://opentdb.com/api.php?amount=2&category=9&difficulty=easy&type=multiple`);
         const data = await response.json();
-        data.results.forEach((data) => {
+        /*data.results.forEach((data) => {
             setQuestion((prevState) => ({ ...prevState, [data.question] : [data.correct_answer] [data.incorrect_answers]}))
             result.push(data);
-        })
+        })*/
+        result = data.results;
         console.log(`Data is: ${data}`);
         setQuestions(result);
     }
@@ -29,21 +30,34 @@ const QuizQuestions = () => {
     useEffect(() => {
         fetchQuestions();
     }, [])
-    console.log(questions)
+    
+    
     //console.log(questions);
-    const renderQuestion = () => {if(questions != undefined){return questions.map((Q) => (
+    /*const renderQuestion = () => {if(questions != undefined){return questions.map((Q) => (
       <>
-        <strong>
+        <strong></strong>
           <p>{Q.question}</p>
         </strong>
         {renderAnswer(Q)}
       </>
-    ));}}
+    ));}}*/
+
+     // assign the rendering of questions, display each question and its answers each time
+     let round = 0;
+
+    const renderQuestion = () => {if(questions != undefined){
+        console.log(questions[round])
+        return <>
+          <strong>
+            <p>{questions[round].question}</p>
+          </strong>
+          {renderAnswer(questions[round])}
+        </>
+      ;}}
 
     // shuffle our array below 'allAnswers' containing all candidate answers                    
     function shuffle(array) {
         let currentIndex = array.length,  randomIndex;
-
         // While there remain elements to shuffle.
         while (currentIndex != 0) {
 
@@ -55,8 +69,24 @@ const QuizQuestions = () => {
             [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]];
         }
-
         return array;
+    }
+
+    let submit = "";
+
+    // handle the answers for every quiz
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(submit == questions[round].correct_answer){
+            console.log(`correct answer ${submit}`)
+        }else{
+            console.log(`the right answer is: ${questions[round].correct_answer}`)
+        }
+    }
+
+    const handleInput = e => {
+        const input = e.target.value;
+        submit = input;
     }
 
     const renderAnswer = (Q) => {if(questions != undefined){
@@ -68,15 +98,31 @@ const QuizQuestions = () => {
         // for(i=0; i<=allAnswers.length; i++){
         //     return allAnswers[i]
         // }
-        return uniqueShuffled.map( (Q) =>
-        <p key={Q.id}>{Q}</p>
-        );}}
+        return (
+            <> 
+                <div className=''>
+                    <form onSubmit={handleSubmit}>
+                        {uniqueShuffled.map( (Q) =>
+                                <>
+                                <input type="radio" id={Q} className ="answer" name="answer" value={Q} onChange={handleInput}/>
+                                <label for={Q}>{Q}</label>
+                                </>       
+                        )}
+                        <input type="submit" value="Submit"/>  
+                    </form>
+                </div>
+                <br></br>
+                  
+            </>
+        )
+        ;}}
     
     
         return (
-            <section role="">
+            <section role="" className="">
                 <>
                 <h2>You Quiz Game:</h2>
+                {/* <h3>You selected <i>{NumOfQuestion}</i> questions of the <i>'{questions[round].category}'</i>  category in <i>{level}</i> difficulty level!</h3> */}
                 <p id="question" name="question" className="question">
                     {renderQuestion()}
                     
