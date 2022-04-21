4b5bf26409374062f5509345880be4ca7eae8393
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-21 19:04:50 +0300

    accepted incoming changes from merge

--- client/src/pages/CreateGame/index.js
+++ client/src/pages/CreateGame/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import './style.css'
 
-<<<<<<< HEAD:client/src/pages/createGame.js
 const CreateGame = () => (
-=======
-import {QuizForm} from "../../components";
-
-function CreateGame() {
-  
-    return(
->>>>>>> development:client/src/pages/CreateGame/index.js
     <>
     
     </>
