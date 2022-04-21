7be5610a1d079dc90a6030872b4c606a07053e56
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-22 01:12:02 +0300

    deployment effort on heroku

--- server/api/controllers/games.js
+++ server/api/controllers/games.js
@@ -3,7 +3,7 @@
 
 // const bodyParser = require ('body-parser');
 
-const Game = require('../models/Game');
+const Game = require('../models/game');
 
 // get all games from the collection
 async function getAll(req, res) {
