f6345df246c06a3f6de451d591754c8f4270382e
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-21 19:01:09 +0300

    accepted incoming changes from merge

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,11 @@
 export { default as Search } from './Search';
-export { default as CreateGame } from './createGame';
+export { default as CreateGame } from './CreateGame';
 export { default as EndGame } from './EndGame';
 export { default as Game } from './Game';
 export { default as HomePage } from './Homepage';
 export { default as Join } from './Join';
 export { default as LeaderBoard } from './LeaderBoard';
 export { default as Lobby } from './Lobby';
-<<<<<<< HEAD
-=======
 export { default as HostLobby } from './HostLobby';
 
 // export {CreateGame} from './CreateGame';
->>>>>>> development
