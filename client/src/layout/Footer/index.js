e94d150ec376159767b814368927626d0631a0ab
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-22 08:28:09 +0100

    added link to notion page for contributors github pages

--- client/src/layout/Footer/index.js
+++ client/src/layout/Footer/index.js
@@ -7,10 +7,13 @@ const Footer = () => {
         <footer class="footer-links">
             <p class="copyright">© SQUIZ 2022 Designed by
               <i>
-                <a rel="noopener noreferrer" href="#" target="_blank"> SquizTeam</a>
+                <a rel="noopener noreferrer" href="https://www.notion.so/SQUIZ-TEAM-e8fcc55f4f7e4e9da9bc79bcc9ce3090" target="_blank"> SquizTeam</a>
               </i>
             </p>
         </footer>
+
+
+
       </>
 
     )
--- client/src/pages/EndGame/index.js
+++ client/src/pages/EndGame/index.js
@@ -9,10 +9,10 @@ const EndGame = () => {
     return <div className='endContainer'>
 
     <h1 className='game-over'>Game Over</h1>
-    <h4 className='game-result'>Game score</h4>
-    <p className="end-game-result">User:{UserName}</p>
+    <h4 className='game-result'>Quiz score</h4>
+    <p className="end-game-result">User: {UserName}</p>
     <br></br>
-    <p className="end-game-result" >Score:{Score}</p>
+    <p className="end-game-result" >Score: {Score}</p>
     <NavButton className="game-over-btn" path="/" value= "Leave game"/>
     <NavButton path="/Create" value= "Play Again"/>
     </div>
