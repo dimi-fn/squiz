8d5f834289f225210a44258c8fa6aada937150ae
HevvsOlivia <heather@mcglynn.me.uk>
2022-04-22 09:52:18 +0100

    CSS changes

--- client/src/components/Form/HomeForm.js
+++ client/src/components/Form/HomeForm.js
@@ -5,7 +5,7 @@ function HomeForm({handleSubmit, updateInput, Create, Join}) {
     return (
         <form id="homeForm" onSubmit={handleSubmit}>
             <label htmlFor="username" id="usernameLabel">ENTER USERNAME</label>
-            <input type="text" id="username" onChange={updateInput}/>
+            <input type="text" required id="username" onChange={updateInput}/>
 
             <input type="submit" id="createRoom" value="Create Room" style={{cursor: 'pointer'}} onClick={Create}/>
             <input type="submit" id="joinRoom" value="Join Room" style={{cursor: 'pointer'}} onClick={Join}/>
